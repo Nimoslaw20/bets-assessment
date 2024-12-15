1398781c2930d94c461fa941526968a5d92760a8
EmmanuelNimo <emmanuel.nimo@getampla.com>
2024-12-15 20:54:01 +0100

    added comments to the objects

--- pages/objects/add-to-cart-objects.js
+++ pages/objects/add-to-cart-objects.js
@@ -1,7 +1,9 @@
+
 const createSelector = (suffix) => `[data-test="${suffix}"]`;
 
-
 module.exports = {
+
+  //using data-test element
   itemElement: `div.inventory_item_name${createSelector('inventory-item-name')}`,
   addToCartButton: createSelector('add-to-cart'),
   countBadge: createSelector('shopping-cart-badge'),
@@ -14,14 +16,18 @@ module.exports = {
   cancelButton: createSelector('cancel'),
   inventoryItemName: createSelector('inventory-item-name'),
   inventoryContainer: createSelector('inventory-container'),
+  backToProducts:createSelector('back-to-products'),
+  continueButton: createSelector('continue'),
+  productImage: createSelector('item-4-img-link'),
+  buttonElement: `button.btn.btn_primary.btn_small.btn_inventory${createSelector('add-to-cart-')}`,
+
+
+  //using class 
   inventorItemNameWithDataTest:'.inventory_item_name',
   inventoryItem:'.inventory_item',
   productDescription: '.inventory_item_desc',
   productName: '.inventory_item_name',
   productPrice:'.inventory_item_price',
-  backToProducts:createSelector('back-to-products'),
-  continueButton: createSelector('continue'),
-  productImage: createSelector('item-4-img-link'),
-  buttonElement: `button.btn.btn_primary.btn_small.btn_inventory${createSelector('add-to-cart-')}`,
+  
 
 };
